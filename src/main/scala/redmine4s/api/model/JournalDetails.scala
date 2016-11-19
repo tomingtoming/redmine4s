4b80e3365bb98f9b3b133342bbb7c569ccec906c
toming <tomingtoming@gmail.com>
2016-11-20 08:40:32 +0900

    Delete unnecessary comments

--- src/main/scala/redmine4s/api/model/JournalDetails.scala
+++ src/main/scala/redmine4s/api/model/JournalDetails.scala
@@ -1,6 +1,3 @@
 package redmine4s.api.model
 
-/**
-  * Created by toming on 16/10/12.
-  */
 case class JournalDetails(property: String, name: String, oldValue: Option[String], newValue: Option[String])
