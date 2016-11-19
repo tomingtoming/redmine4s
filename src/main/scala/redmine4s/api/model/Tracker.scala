49a17a7e5a79cb0927e72b175d582be3e0fcbede
toming <tomingtoming@gmail.com>
2016-11-20 02:03:37 +0900

    Tracker does not need Redmine instance

--- src/main/scala/redmine4s/api/model/Tracker.scala
+++ src/main/scala/redmine4s/api/model/Tracker.scala
@@ -1,5 +1,3 @@
 package redmine4s.api.model
 
-import redmine4s.Redmine
-
-case class Tracker(id: Long, name: String, defaultStatus: Option[(Long, String)], redmine: Redmine)
+case class Tracker(id: Long, name: String, defaultStatus: Option[(Long, String)])
