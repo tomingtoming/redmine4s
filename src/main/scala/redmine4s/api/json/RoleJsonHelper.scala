03447f1bf0c77dbcc6c68c24446d8b8aaf9c0331
toming <tomingtoming@gmail.com>
2016-11-20 10:21:53 +0900

    Classify Permission of Role

--- src/main/scala/redmine4s/api/json/RoleJsonHelper.scala
+++ src/main/scala/redmine4s/api/json/RoleJsonHelper.scala
@@ -2,15 +2,14 @@ package redmine4s.api.json
 
 import play.api.libs.functional.syntax._
 import play.api.libs.json._
-import redmine4s.api.model.Role
+import redmine4s.api.model.{Permission, Role}
 
 trait RoleJsonHelper extends BaseJsonHelper {
+  implicit val permissionReads: Reads[Permission] = __.read[String].map(Permission.fromString)
   implicit val roleReads: Reads[Role] = (
     (__ \ 'id).read[Long] ~
       (__ \ 'name).read[String] ~
       (__ \ 'inherited).readNullable[Boolean] ~
-      (__ \ 'permissions).readNullable[Iterable[String]]
-    ) { (id: Long, name: String, inherited: Option[Boolean], permissions: Option[Iterable[String]]) =>
-    Role(id, name, inherited, permissions, null)
-  }
+      (__ \ 'permissions).readNullable[Iterable[Permission]]
+    ) { (id, name, inherited, permissions) => Role(id, name, inherited, permissions, null) }
 }
